58282a13a74a4bc6da420832f5ed5d9084eed70b
Jonathan <jon_sousapires@hotmail.com>
2026-04-08 22:20:23 -0300

    Praticando com um exercicio a utilização de herança em POO

--- exercicios/exercicioPOO.py
+++ exercicios/exercicioPOO.py
@@ -1,1 +1,14 @@
+class Funcionario:
+    def __init__(self, nome, identidade, setor, salario):
+        self.nome = nome
+        self.identidade = identidade
+        self.setor = setor
+        self.salario = salario
 
+    def __repr__(self):
+        return '(' + str(self.nome) + ' ,' + str(self.identidade) + ' ,' + str(self.setor) + ' ,' + str(self.salario) + ','
+
+class Gerente(Funcionario):
+
+    def __repr__(self):
+        return '(' + str(self.nome) + ' ,' + str(self.identidade) + ' ,' + str(self.setor) + ' ,' + str(self.salario + self.salario * 0.15) + ','
